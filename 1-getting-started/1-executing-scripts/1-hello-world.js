ae0da4d33ec0cefa18690b2391a104cc3033cb0f
Romain <rmtestard@gmail.com>
2018-12-04 09:16:08 +0100

    modern js

--- 1-getting-started/1-executing-scripts/1-hello-world.js
+++ 1-getting-started/1-executing-scripts/1-hello-world.js
@@ -1,9 +1,13 @@
+// requiere the http dependency
 const http = require('http');
 
+// http dependency to create a server
 const server = http.createServer((req, res) => {
-  res.end('Hello World\n');
+  // response object, otherside of the request
+  res.end('Hello Node\n');
 });
 
+// to activate the server, you have to listen to it
 server.listen(4242, () => {
   console.log('Server is running...');
 });
