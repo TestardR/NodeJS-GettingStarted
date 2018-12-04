0ea64cb38db97a6537eb20b4db8a266344a25b59
Romain <rmtestard@gmail.com>
2018-12-04 12:32:44 +0100

    res and req

--- 5-web/1-hello-world.js
+++ 5-web/1-hello-world.js
@@ -1,8 +1,14 @@
 const http = require('http');
 
-const server = http.createServer((req, res) => {
+const requestListiner = (req, res) => {
+  // req and res are streams !
+  console.dir(req, { depth: 0 }); // Type: Incoming Message
+  console.dir(res, { depth: 0 }); // Type: Server Response
   res.end('Hello World\n');
-});
+};
+
+const server = http.createServer();
+server.on('request', requestListiner);
 
 server.listen(4242, () => {
   console.log('Server is running...');
